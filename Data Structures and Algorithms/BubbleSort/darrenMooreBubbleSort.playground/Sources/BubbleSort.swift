c4b0d8d686d5414beff7acce4793cc7190b00027
darren <darrenm95@icloud.com>
2022-03-17 12:46:19 +0000

    Update swift bubble sort solution

--- Data Structures and Algorithms/BubbleSort/darrenMooreBubbleSort.playground/Sources/BubbleSort.swift
+++ Data Structures and Algorithms/BubbleSort/darrenMooreBubbleSort.playground/Sources/BubbleSort.swift
@@ -1,16 +1,24 @@
-public func BubbleSort<Element: Comparable>(_ dataset: [Element]) -> [Element] {
-    var array = dataset
+/// Performs the Bubble sort algorithm to a given array of elements
+///
+///  - Parameter unsortedArray: The array to be sorted, containing elements that conform to the Comparable protocol
+///  - Returns: A sorted array of the same elements
+public func BubbleSort<Element: Comparable>(_ unsortedArray: [Element]) -> [Element] {
+    // Exit early if array of length 1, the array is already sorted
+    guard unsortedArray.count > 1 else { return unsortedArray }
+    
+    /// - array: copy the unsortedArray to save stability
+    var array = unsortedArray
     var swaps = false
     var numberOfRepeats = 0
     
     repeat {
         swaps = false
-        for i in 1 ..< array.count - numberOfRepeats {
+        for index in 1 ..< array.count - numberOfRepeats {
     
-            if array[i] < array[i - 1] {
-                let temp = array[i - 1]
-                array[i - 1] = array[i]
-                array[i] = temp
+            if array[index - 1] > array[index] {
+                let temp = array[index - 1]
+                array[index - 1] = array[index]
+                array[index] = temp
                 
                 swaps = true
             }
