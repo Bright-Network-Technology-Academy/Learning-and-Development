51906b3fe22b7ee59eb00ad030d05c54142d2288
Vinh Chu <70478106+vinhchugg@users.noreply.github.com>
2022-03-28 16:33:11 +0100

    Add quick sort solution -vinh

---
+++ Data Structures and Algorithms/QuickSort/VinhChuQuickSort.js
@@ -0,0 +1,23 @@
+function pivot(arr, start = 0, end=arr.length + 1) {
+    let pivot = arr[start];
+    let swapIndex = start;
+
+    for(let i = start + 1; i < arr.length; i++) {
+        if(pivot > arr[i]) {
+            swapIndex++;
+            [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]];
+        }
+    }
+    [arr[start], arr[swapIndex]] = [arr[swapIndex], arr[start]];
+    return swapIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if(left < right) {
+        let pivotIndex = pivot(arr, left, right);
+
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
